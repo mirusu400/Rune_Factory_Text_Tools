89f22a8978f66516c42959246864c56426bb00c8
mirusu400 <mirusu400@naver.com>
2017-07-30 00:26:06 +0900

    완성
    내일보면 다까먹을거같아요 시밤

--- RF WQSG Text Dump with TBL.py
+++ RF WQSG Text Dump with TBL.py
@@ -1,11 +1,16 @@
 # -*- coding:utf-8 -*-
+"""
+-------------------구조
+맨 처음 포인터를 찾고(foundoffset) 대사를 찾은후 포인터를 찾고(nextoffset) 이를 txt에 기록합니다.
+유니코드일경우 TBL을 탐색하여 그 TBL에 해당하는 글자가 있으면 그 TBL로 추출하고 없을경우 오류메세지를 띄우는 기능입니다(예정)
+"""
+
 global readfile
 global writefile
 global tablefile
 global inFp3
-global TBLword
-global TBLhex
 
+import time
 import sys
 
 
@@ -84,9 +89,20 @@ def nextoffset(nextoffset):
     return result
 
 def tableread():
-    inFp4=open(tablefile,"r")
-
-
+    global TBLword
+    global TBLhex
+    inFp4=open(tablefile,"r",encoding='utf-16-le')
+    while True:
+        line=inFp4.readline()
+        line=line.replace("\n","")
+        line=line.split("=")
+        if (line == ['']):
+            break
+        line[0] = line[0].replace(u"\ufeff", '') #BOM고유오류 조정
+        TBLword.append(line[1])
+        TBLhex.append(line[0])
+    print(TBLword)
+    print(TBLhex)
     return
 
 readfile=sys.argv[1]
@@ -103,9 +119,9 @@ shortoffset=0 #한번 찾으면 바로 초기화
 TBLword = [] #테이블 파일중 단어
 TBLhex = [] #테이블 파일중 16진수값
 startoffset=foundoffset(inFp) #오프셋찾기(0x0c)
-
+tableread() #테이블파일읽기
 inFp=open(readfile,"rb")
-outFp=open(writefile,"w")
+outFp=open(writefile,"w",encoding='utf-16-le')
 inFp3 = open(readfile, "rb")
 inFp3.read(0xC)
 
@@ -121,6 +137,7 @@ while True:
     if s == '':
         break
     if(ord(s)==00): #마지막일경우
+        result=""
         if(lenscrpit==0):
             resultoffset = nextoffset(0)
         else:
@@ -130,18 +147,86 @@ while True:
         a=inFp2.read(startoffset) #처음 커서까지 이동
         if(longoffset!=0):
             a = inFp2.read(longoffset)  # 방금까지의 오프셋으로 이동
-            a = inFp2.read(shortoffset)  # 총 대사길이 읽기
+            length=0
+            tempoffset = shortoffset
+            while True:  # 총 대사길이 읽기
+                if(length==tempoffset):
+                    break
+                a = inFp2.read(1)
+                a = str(a)
+                if(len(a)==7) and not(a==str(b'\x00'))  and not(a=="b'\\xc'") and not(a==str(b'\xa0')): #ASCII 코드에 없는경우
+                    tblresult="" #tbl 찾는값
+
+                    tempoffset-=2 #2개를 더읽어오므로 미리추가
+                    b = inFp2.read(2)
+                    b = str(b)
+                    tblresult+=a[3:]
+                    tblresult+=b[3:]
+                    tblresult = tblresult.replace("'", "")
+                    tblresult = tblresult.replace("x", "")
+                    tblresult = tblresult.replace("\\", "")
+                    tblresult=tblresult.upper() #테이블 파일을 읽기위해 3바이트 HEX로 치환
+                    check=0
+                    for i in range(0,len(TBLhex)): #3바이트 HEX가 테이블파일에 있는지 검사
+                        if(tblresult == str(TBLhex[i])):
+                            tblresult = str(TBLword[i])
+                            check=1
+                            print(tblresult)
+                            break
+                    if(check==0):
+                        talk="TBL FILE CANNOT FOUND HEX CODES IN SCRPIT!"
+                        talk+=tblresult
+                        print(talk)
+                        break
+
+                    result+=tblresult
+                else:
+                    result+=a[2:-1]
+                length+=1
+
         else:
-            a = inFp2.read(shortoffset)  # 총 대사길이 읽기
+            length=0
+            tempoffset = shortoffset
+            while True:  # 총 대사길이 읽기
+                if(length==tempoffset):
+                    break
+                a = inFp2.read(1)
+                a = str(a)
+                if(len(a)==7) and not(a==str(b'\x00'))  and not(a=="b'\\xc'") and not(a==str(b'\xa0')): #ASCII 코드에 없는경우
+                    tblresult="" #tbl 찾는값
+                    tempoffset-=2 #2개를 더읽어오므로 미리추가
+                    b = inFp2.read(2)
+                    b = str(b)
+                    tblresult+=a[3:]
+                    tblresult+=b[3:]
+                    tblresult = tblresult.replace("'", "")
+                    tblresult = tblresult.replace("x", "")
+                    tblresult = tblresult.replace("\\", "")
+                    tblresult=tblresult.upper() #테이블 파일을 읽기위해 3바이트 HEX로 치환
+                    check=0
+                    for i in range(0,len(TBLhex)): #3바이트 HEX가 테이블파일에 있는지 검사
+                        if(tblresult == str(TBLhex[i])):
+                            tblresult = str(TBLword[i])
+                            check=1
+                            break
+                    if(check==0):
+                        talk="TBL FILE CANNOT FOUND HEX CODES IN SCRPIT!"
+                        talk+=tblresult
+                        print(talk)
+                        break
+
+                else:
+                    result+=a[2:-1]
+                length += 1
         longoffset+=shortoffset+1 #방금까지 오프셋 추가
         a=str(a)
 
-        print(a[1:])
+
         outFp.write(str(resultoffset))
         outFp.write(",")
         outFp.write(str(shortoffset))
         outFp.write(",")
-        outFp.write(a[2:-1])
+        outFp.write(result)
         outFp.write("\n")
         a=inFp.read(1)
         shortoffset = 0  # 대사길이 초기화
