cd119f70812ce98fd9c2720001dfdc53ba33662e
mirusu400 <mirusu400@naver.com>
2017-09-03 16:26:24 +0900

    WIP2
    WIP

--- RF Script INPUT TOOL.py
+++ RF Script INPUT TOOL.py
@@ -19,10 +19,9 @@ def string_hex_to_hex(temps): #string안에 저장된 hex값을 읽어와 쓰게
     for i in range(0, int(len(temps)/2)):
 
         outtemp = int(temps[i * 2:i * 2 + 2], 16)
-        print(temps[i * 2:i * 2 + 2])
+        #print(temps[i * 2:i * 2 + 2])
         outtemp2 = struct.pack("B", outtemp)
         outFp.write((outtemp2))
-
 def tableread():
     global TBLword
     global TBLhex
@@ -39,7 +38,7 @@ def tableread():
     print(TBLword)
     print(TBLhex)
 
-    return
+    return #TBL파일을 읽어옵니다
 def countline():
     tempFp = open(readfile, "r",encoding="utf-8")
     temp=0
@@ -48,14 +47,14 @@ def countline():
         if not line: break
         if(line[0]!="\n"): #공백 개행일 경우 넘어가버립니다.
             temp+=1
-    return temp
+    return temp#라인의 총 갯수를 셉니다 #라인의 총 갯수를 셉니다
 def big_to_little_end(tempinput): #빅 엔디안으로 계산된걸 룬팩토리2에 읽을수있게 스몰 엔디안으로 만들어줍니다
     if(len(tempinput)==1):
         output="0"+tempinput+"000000"
     if(len(tempinput)==2):
         output=tempinput[0:2]+"000000"
     if(len(tempinput)==3): #123 -> 23010000
-        print("hi")
+        
         output=tempinput[1:3]+"0"+tempinput[0]+"0000"
     if(len(tempinput)==4):
         output=tempinput[2:]+tempinput[0:2]+"0000"
@@ -72,30 +71,73 @@ def big_to_little_end(tempinput): #빅 엔디안으로 계산된걸 룬팩토리
     return output
 
 
-readfile=sys.argv[1]
+#readfile=sys.argv[1]
+readfile="C:\\Users\\Jun Fac porta03\\Desktop\\rf2TxtCalendar.txt"
+
 try:
     writefile = sys.argv[2]
 except:
     writefile=readfile
     writefile+=".out"
-tablefile=sys.argv[3]
+#tablefile=sys.argv[3]
+tablefile="수정UTF_수정_실전_대사삽입용.tbl"
 inFp=open(readfile,"r",encoding="utf-8")
 outFp=open(writefile,"wb")
+
 texts=[]
 TBLword = [] #테이블 파일중 단어
 TBLhex = [] #테이블 파일중 16진수값
-tableread() #테이블파일읽기
-tpcountline=countline()
+WQSG = 1 #-----------------------------------------------------WQSG사용을 하는경우 반드시 1로 설정해주세요
 
+tableread() #테이블파일읽기
+tpcountline=countline() # 총 라인수(불변)
+print(tpcountline)
+tempcountline=tpcountline #총 라인수(가변)
 lentexts=hex(tpcountline)
 lentexts=(big_to_little_end(lentexts[2:]))
-print(lentexts)
+#print(lentexts)
 outlabel=b'TEXT'
 outFp.write(outlabel)
-string_hex_to_hex(lentexts) #스트링에 있는값을 그대로 씁니다
-string_hex_to_hex("00000000")
+string_hex_to_hex(lentexts) #------------------------------텍스트의 총 갯수를  씁니다
+#string_hex_to_hex("00000000")  #---------------------------------- 텍스트의 길이를 사용해야합니다
+#--------------inFp를 읽으면서 한 글자씩 써야 합니다
+print(tpcountline)
+for a in range(0,tpcountline):
+	openline=inFp.readline()
+	print(openline)
+	if(WQSG == 1):
+		templines=openline.split(",")
+		print(templines)
+		openline=""
+		for i in range(0,len(templines)-2):
+			openline+=templines[i+2] #--------------앞의 두개의 ,를 생략해 저장합니다(WQSG 대비)
+			openline+=","
+	openline=openline.replace("\n","")
+	openline=openline[0:-1]
+	print(openline)
+	result=0
 
-tpcountline=hex(tpcountline * 8+8)
-tpcountline=big_to_little_end(tpcountline[2:])
-string_hex_to_hex(tpcountline)
+	for i in range(0,len(openline)):
+		for k in range(0,len(TBLword)):
+			if(openline[i] == TBLword[k]):
+				result+=len(TBLhex[k])/2
+				print(int(result))
+	print("result")
+	print(result)
+	time.sleep(2)
+	string_hex_to_hex((big_to_little_end(str(hex(int(result)))[2:]))) #-------------최종 결과값을 결과에 씁니다
+	if(a == 0):
+		tempcountline=hex(tpcountline * 8+8) #첫번째 오프셋 계산()
+		print(tempcountline)
+		countlineresult=big_to_little_end(str(tempcountline)[2:]) #-------------첫번째 오프셋의 경우 직접 계산합니다
+		string_hex_to_hex(countlineresult)
+	else:
+		print((tempcountline[2:]))
 
+		print(int(tempcountline,16)) #hex값을 10진수로 변환(작동x)
+		tempcountline=hex(int(tempcountline,16)+(int(result))) #hex값을 10진수로 계산해 더한다음 다시 16진수로 변환한후 이를 ROM에 기록
+		print(tempcountline)
+		time.sleep(100000)
+
+
+time.sleep(1)
